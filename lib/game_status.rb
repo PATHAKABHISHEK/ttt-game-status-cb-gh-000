dceefd7894be2d8b16ba76a6d40943d1487dd1b3
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-05 13:28:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,7 +47,18 @@ def full?(board)
 
 if(won?(board) == false)
   return true
-
+end
+if true
+  while (i < 9)
+    if(position_taken?(board,i))
+      j = j + 1
+    end
+     i = i + 1
+  end
+if ( j != 8)
+  return false
+end
+end
 
 end
 #below is over method

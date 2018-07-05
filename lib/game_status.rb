966633202ec468f07eb3a2f8e85c0207d34553cc
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-05 13:01:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,17 +40,18 @@ end
 # below is full method
 
 def full?(board)
+i = 0
 j = 0
 while i < 9
-if(position_taken?(board,i = 0))
+if(position_taken?(board,i))
   j = (j + 1)
   end
  i = (i + 1)
 end
   if(j == 8)
-    return true
-  else
     return false
+  else
+    return true
   end
 end
 
